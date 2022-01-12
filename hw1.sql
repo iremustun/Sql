3ff703f8f318a50db749f61d73e19bbed2ee9f77
iremustun <81117568+iremustun@users.noreply.github.com>
2022-01-12 23:44:55 +0300

    Update hw1.sql

--- hw1.sql
+++ hw1.sql
@@ -5,7 +5,7 @@ SELECT * FROM film WHERE length BETWEEN 60 AND 75;
 SELECT first_name, last_name FROM customer WHERE first_name='Mary';
 
 SELECT * FROM film 
-WHERE rental_rate=0.99 AND replacement_cost=12.99 OR replacement_cost=28.99;
+WHERE rental_rate=0.99 AND replacement_cost IN (12.99,28.99);
 
 SELECT * FROM film 
 WHERE (NOT length>50) AND NOT(rental_rate=2.99 OR rental_rate=4.99);
